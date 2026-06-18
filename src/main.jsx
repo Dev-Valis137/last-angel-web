6e012598901c2eb74c9f4f60867037da0957efbc
Dev-Valis137 <dev@valis137.com>
2026-06-18 19:25:45 -0400

    Add smooth cursor effect with smooth-cursor + framer-motion

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom/client'
+import { SmoothCursor } from 'smooth-cursor'
 import GravityStarsBackground from './GravityStarsBackground.jsx'
 import ThemeTogglerButton from './ThemeTogglerButton.jsx'
 
 ReactDOM.createRoot(document.getElementById('react-root')).render(
-  <GravityStarsBackground />
+  <>
+    <GravityStarsBackground />
+    <SmoothCursor />
+  </>
 )
 
 const toggleTarget = document.getElementById('theme-toggle-target')
